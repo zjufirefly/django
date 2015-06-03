b1d64c6c7635a8b55ff3160e7bdc9e27dd40c896
fanbin <fanbin@jd.com>
2015-06-03 10:12:04 +0800

    home page

--- metasp/settings.py
+++ metasp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'metasp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [externals, templates],
+        'DIRS': ['externals', 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
