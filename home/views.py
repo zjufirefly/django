4abf45875798653a09f70d96d80139ac8cda8762
fanbin <fanbin@jd.com>
2015-06-03 20:17:08 +0800

    add server, pass views arg to template

--- home/views.py
+++ home/views.py
@@ -4,8 +4,32 @@ from django.http import HttpResponse
 
 def index(request):
 #    return HttpResponse("Hello World")
-    print request.path
-    return render(request, 'index.html')
+    return render(request, 'base.html')
+
+
+class server:
+    def __init__(self):
+        self.server_id = 0
+        self.server_ip = "0.0.0.0"
+        self.server_port = 3306
+
+def servers(request):
+    serv1 = server()
+    serv1.server_id = 1;
+    serv1.server_ip = "10.9.3.241"
+    serv1.port = 3306
+
+    serv2 = server()
+    serv2.server_id = 2;
+    serv2.server_ip = "10.9.3.242"
+    serv2.port = 3306
+
+    servs = []
+    servs.append(serv1)
+    servs.append(serv2)
+
+    return render(request, 'servers.html', {'servs': servs})
+
 
 def all(request):
     return render(request, request.path[1:])
--- metasp/urls.py
+++ metasp/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'home.views.index'),
+    url(r'^servers', 'home.views.servers'),
     url(r'.*', 'home.views.all'),
 ]
